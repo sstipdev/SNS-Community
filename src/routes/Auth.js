f22a6b61fb50e6b32dcc6ff6a4fd8d0c12ed2d07
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-06 23:28:51 +0900

    로그인, 폼 구현

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>인증</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} name="email" type="text" placeholder="이메일" required value={email} />
+        <input onChange={onChange} name="password" type="password" placeholder="비밀번호" required value={password} />
+        <input type="submit" value="로그인" />
+      </form>
+      <div>
+        <button>Google 로그인</button>
+        <button>Gitgub 로그인</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
