05248581ea496dd8849e905de1ba126d95d7329b
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-06 22:22:45 +0900

    firebase 설치 셋팅

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUE98ER7LT4XFEY1yKKC3tfADEk7okW9Q",
+  authDomain: "sns-community.firebaseapp.com",
+  projectId: "sns-community",
+  storageBucket: "sns-community.appspot.com",
+  messagingSenderId: "270856336684",
+  appId: "1:270856336684:web:6b9f65c8e5d2f2b29f2889",
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
