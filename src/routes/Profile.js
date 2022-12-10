870fbb0eae98b81b80bc503a024021e707a6d7ff
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-10 17:17:20 +0900

    프로필 정보 받아오기

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,25 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import React, { useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
+import { collection, getDocs, orderBy, query, where } from "@firebase/firestore";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   let history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const getMySns = async () => {
+    // where 로 필터링을 할수있다.
+    const q = query(collection(dbService, "SNS"), where("creatorId", "==", userObj.uid), orderBy("createdAt"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+  useEffect(() => {
+    getMySns();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>로그아웃</button>
