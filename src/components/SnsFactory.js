e24160a1bfab55b104f26592edac33d659ce0479
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-12-20 21:32:29 +0900

    이미지파일 버킷에 업로드

--- src/components/SnsFactory.js
+++ src/components/SnsFactory.js
@@ -1,7 +1,9 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import React, { useState } from "react";
 import { TextField, Button } from "@mui/material";
 import { addDoc, collection } from "firebase/firestore";
+import { ref, uploadString } from "@firebase/storage";
+import { v4 as uuidv4 } from "uuid";
 
 const SnsFacktory = ({ userObj }) => {
   const [sns, setSns] = useState("");
@@ -9,18 +11,19 @@ const SnsFacktory = ({ userObj }) => {
   const onSubmit = async (e) => {
     e.preventDefault();
     // 현재 firebase 버킷생성 구글오류가 계속 출력되어 이미지업로드 기능구현은 불가
-    // const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
-    // const response = await uploadString(fileRef, file, "data_url");
+    const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+    const response = await uploadString(fileRef, attachment, "data_url");
     // const fileUrl = await response.ref.getDownloadURL();
+    console.log(response);
 
     // 파이어베이스 Missing or insufficient permissions 오류가 확인되어 firstore 규칙을 변경해서 해당 오류를 해결함
-    await addDoc(collection(dbService, "SNS"), {
-      text: sns,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-      userName: userObj.displayName,
-      userEmail: userObj.email,
-    });
+    // await addDoc(collection(dbService, "SNS"), {
+    //   text: sns,
+    //   createdAt: Date.now(),
+    //   creatorId: userObj.uid,
+    //   userName: userObj.displayName,
+    //   userEmail: userObj.email,
+    // });
     setSns("");
   };
 
